b5f2a73706ede49cc7d92ce3496f7148f183ac6a
Adwaith Jayasankar <kichu31084@gmail.com>
2024-08-16 23:40:11 +0530

    Update: Made model class for audio recordings

--- lib/models/audio_recording.dart
+++ lib/models/audio_recording.dart
@@ -1,1 +1,26 @@
 // Created by: Adwaith Jayasankar, Created at: 16-08-2024 23:12
+import 'package:hive/hive.dart';
+
+part 'audio_recording.g.dart'; // Hive generates this file
+
+@HiveType(typeId: 0)
+class AudioRecording {
+  @HiveField(0)
+  final String name;
+
+  @HiveField(1)
+  final String path;
+
+  @HiveField(2)
+  final DateTime date;
+
+  @HiveField(3)
+  final String duration;
+
+  AudioRecording({
+    required this.name,
+    required this.path,
+    required this.date,
+    required this.duration,
+  });
+}
