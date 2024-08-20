71549017c84be299812f32d5af7a3c5bd15236a6
Adwaith Jayasankar <kichu31084@gmail.com>
2024-08-20 17:43:38 +0530

    Update: Added the route for splash screen

--- lib/routes.dart
+++ lib/routes.dart
@@ -6,10 +6,12 @@ import 'package:koode_v2/ui/screens/default_audio/alphabets/alphabet_page.dart';
 import 'package:koode_v2/ui/screens/default_audio/default_audio_list.dart';
 import 'package:koode_v2/ui/screens/home_page.dart';
 import 'package:koode_v2/ui/screens/recordings_list/recordings_list_page.dart';
+import 'package:koode_v2/ui/screens/splash_screen.dart';
 
 class AppRoutes {
   static Map<String, WidgetBuilder> routes = {
-    '/': (context) => const HomePage(),
+    '/': (context) => const SplashScreen(), // New SplashScreen route
+    '/home': (context) => const HomePage(), // HomePage moved to '/home'
     '/default-audios': (context) => const DefaultAudiosPage(),
     '/alphabet-list': (context) => const AlphabetListPage(),
     '/recordings-list': (context) => const RecordingsListPage(),
