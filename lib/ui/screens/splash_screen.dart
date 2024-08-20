daddf7c93f337a4d86aa10f8a2ebd16cf892ccff
Adwaith Jayasankar <kichu31084@gmail.com>
2024-08-20 17:43:58 +0530

    Update: Made a new file for a splash screen

---
+++ lib/ui/screens/splash_screen.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'home_page.dart'; // Make sure to import your home page
+import 'package:google_fonts/google_fonts.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  SplashScreenState createState() => SplashScreenState();
+}
+
+class SplashScreenState extends State<SplashScreen> with SingleTickerProviderStateMixin {
+  late AnimationController _controller;
+  late Animation<double> _animation;
+
+  @override
+  void initState() {
+    super.initState();
+
+    // Initialize the animation controller and the fade animation
+    _controller = AnimationController(
+      duration: const Duration(seconds: 1),
+      vsync: this,
+    );
+
+    _animation = CurvedAnimation(parent: _controller, curve: Curves.easeIn);
+
+    // Start the animation
+    _controller.forward();
+
+    // Navigate to the home screen after the animation is complete
+    _navigateToHome();
+  }
+
+  @override
+  void dispose() {
+    _controller.dispose();
+    super.dispose();
+  }
+
+  _navigateToHome() async {
+    await Future.delayed(const Duration(seconds: 2), () {});
+    if (mounted) {
+      Navigator.pushReplacement(
+        context,
+        PageRouteBuilder(
+          pageBuilder: (context, animation1, animation2) => const HomePage(),
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            return FadeTransition(opacity: animation, child: child);
+          },
+          transitionDuration: const Duration(seconds: 1),
+        ),
+      );
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: const Color(0xFF071952), // Updated background color
+      body: FadeTransition(
+        opacity: _animation,
+        child: Center(
+          child: Text(
+            'Koode',
+            style: GoogleFonts.balthazar(
+              textStyle: const TextStyle(
+                fontSize: 48,
+                fontWeight: FontWeight.bold,
+                color: Color(0xFF97FEED), // Updated text color
+              ),
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
